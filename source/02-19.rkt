02b8cd34eb79555b0e0a94b48a1c585cd113cc8e
Cheng Lian <lian@databricks.com>
2017-05-15 17:37:33 -0700

    Add debugging code for 02-19

--- source/02-19.rkt
+++ source/02-19.rkt
@@ -33,9 +33,24 @@
     `(,root ,lhs ,rhs*)))
 ;; end
 
-(let* [(t0 (number->bintree 1))
-       (t1 (insert-to-left 2 t0))
-       (t2 (insert-to-right 3 t1))]
+(define (print-bintree t)
+  (let recurse ([t t] [depth 0])
+    (define (print-current-element)
+      (let [(indent (make-string depth #\tab))]
+        (eopl:printf "~a~a~n" indent (current-element t))))
+    (cond [(null? t) '()]
+          [else
+            (recurse (move-to-right-son t) (+ depth 1))
+            (print-current-element)
+            (recurse (move-to-left-son t) (+ depth 1))])))
+
+(let* ([t0 (number->bintree 1)]
+       [t1 (insert-to-left 2 t0)]
+       [t2 (insert-to-right 3 t1)])
+  (print-bintree t0)
+  (print-bintree t1)
+  (print-bintree t2)
+
   (check-equal? t0 '(1 () ()))
   (check-eqv? (current-element t0) 1)
 
